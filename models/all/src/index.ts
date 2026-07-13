52ce797561f261bc097ba587bcf072b1109f6f02
karthikeyan <karthikeyan@youngglobes.com>
2026-07-13 13:55:28 +0800

    spike: register yg-timesheet model in model-all

--- models/all/src/index.ts
+++ models/all/src/index.ts
@@ -31,6 +31,7 @@ import { desktopPreferencesId, createModel as desktopPreferencesModel } from '@h
 import { driveId, createModel as driveModel } from '@hcengineering/model-drive'
 import gmail, { gmailId, createModel as gmailModel } from '@hcengineering/model-gmail'
 import { guestId, createModel as guestModel } from '@hcengineering/model-guest'
+import { ygTimesheetId, createModel as ygTimesheetModel } from '@hcengineering/model-yg-timesheet'
 import hr, { hrId, createModel as hrModel } from '@hcengineering/model-hr'
 import inventory, { inventoryId, createModel as inventoryModel } from '@hcengineering/model-inventory'
 import lead, { leadId, createModel as leadModel } from '@hcengineering/model-lead'
@@ -567,7 +568,8 @@ export default function buildModel (): Builder {
     [serverTrainingModel, serverTrainingId],
     [serverDocumentsModel, serverDocumentsId],
     [serverAiBotModel, serverAiBotId],
-    [serverProcessModel, serverProcessId]
+    [serverProcessModel, serverProcessId],
+    [ygTimesheetModel, ygTimesheetId]
   ]
 
   for (const [b, id, config] of builders) {
